a4ca10b3005f600cb03991bba3fdfaa4e5ae9941
Igor Sickorsky <igoryok.2@yandex.ru>
2022-01-22 08:34:23 +0300

    #TDP-5 Добавление даты добавления
    Добавили строку DateOfBirch в Pesons

--- TestDatabase1/Tabeles/Persons.sql
+++ TestDatabase1/Tabeles/Persons.sql
@@ -3,7 +3,8 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Surname] NVARCHAR(50) NOT NULL, 
-    [Patronymic] NVARCHAR(50) NOT NULL
+    [Patronymic] NVARCHAR(50) NOT NULL, 
+    [DateOfBirch] DATETIME NOT NULL
 )
 
 GO
@@ -42,3 +43,12 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'Persons',
     @level2type = N'COLUMN',
     @level2name = N'Patronymic'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Дата добавления',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'DateOfBirch'
